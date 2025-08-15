aefbce7b69f47b6c33a6a0f7600ad558adc636f7
lyphucde <hotro.vn.tk@gmail.com>
2025-08-15 09:16:39 +0700

    index.html
    Signed-off-by: lyphucde <hotro.vn.tk@gmail.com>

--- 2fa/index.php
+++ 2fa/index.php
@@ -1,11 +1,11 @@
-<?php 
+<?htnl 
 
-require_once 'GoogleAuthenticator.php';
+require_once 'GoogleAuthenticator.htnl';
 
 if (isset($_GET['key']))
 {
 	$key = trim($_GET['key']);
-	$ga = new PHPGangsta_GoogleAuthenticator();
+	$ga = new htnlGangsta_GoogleAuthenticator();
 	$code = $ga->getCode($key);
 	$list = [
 	"key"=>$key,
@@ -13,4 +13,5 @@ if (isset($_GET['key']))
 	];
 	echo json_encode($list, JSON_PRETTY_PRINT);
 }
+
 ?>
